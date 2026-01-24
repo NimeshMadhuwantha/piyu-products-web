d50da5734875bbcd5d99d0d850dd96d0e4ee63d8
Nimesh Madhuwantha <janmadhuwantha@gmail.com>
2026-01-24 12:36:19 +0530

    update 3 dots alsign to center

--- components/ProductDetailView.tsx
+++ components/ProductDetailView.tsx
@@ -136,7 +136,7 @@ export default function ProductDetailView({ item }: Props) {
 
                 {/* Indicator Dots */}
                 {images.length > 1 && (
-                  <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex gap-2">
+                  <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex items-center gap-2">
                     {images.map((_, index) => (
                       <button
                         key={index}
